b697bd055cdbc3b4b4d36a7b61b951dc0d712ca3
Abdullah khalid <127050435+abdullah2565@users.noreply.github.com>
2024-10-18 18:00:20 +0500

    arrow function filter

---
+++ index.js
@@ -0,0 +1,18 @@
+// arrow-function
+
+const abdulah = (username) => {
+    console.log(username);
+    return "hello" +  username;
+
+}
+let greeting = abdulah("abdullah")
+console.log(greeting);
+
+// filter 
+const friut =  ["apple" , "mango" , "banana" , "pineapple"]
+const check = friut.filter((item) => {
+    item == "apple"
+    console.log(item);
+    return 
+})
+console.log(check);
